44e0292d92b56fb626dc07f103221ed4e48e4cb6
khomsan paphue <khomsan.pap@stc.local>
2022-06-01 14:23:06 +0700

    first commit

---
+++ src/resources/views/email/test.blade.php
@@ -0,0 +1,1 @@
+Wellcome testing Maillhog
---
+++ src/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Test Redis
+Route::get('/store', function() {
+    Redis::set('Bangkok', 'Krung Thep Maha Nakhon');
+});
+
+Route::get('/retrieve', function() {
+    return Redis::get('Bangkok');
+});
+
+// Test Mailhog
+Route::get('/send-email', function() {
+    Mail::to('khomsan.pap@thaidev.co')->send(new TestMail);
+});
